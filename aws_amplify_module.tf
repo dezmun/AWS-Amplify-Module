8bc204fc6a3ce6ff29a1db91e56f88928793ecca
Seth Arms <arms.seth@gmail.com>
2022-07-24 07:18:34 -0500

    Gen'd README

--- aws_amplify_module.tf
+++ aws_amplify_module.tf
@@ -1,10 +1,9 @@
 resource "aws_amplify_app" "this" {
   name = "var.appName"
   repository = "var.repoLink"
-
-  //If you create a new Amplify App with the repository argument, you also need to set oauth_token or access_token for authentication.
-  access_token = ""
+  access_token = "EMPTY"
 }
 
-//REGISTRY LINK
-//https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/amplify_app
+/*If you create a new Amplify App with the repository argument, you also need to set oauth_token or access_token for authentication.
+REGISTRY LINK
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/amplify_app*/
