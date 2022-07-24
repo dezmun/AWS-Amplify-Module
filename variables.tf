db6d686b4b5611be5b568a98967988da9acd4abe
Seth Arms <arms.seth@gmail.com>
2022-07-24 06:26:15 -0500

    Created .gitignore and updated variables.tf with PAT variable

--- variables.tf
+++ variables.tf
@@ -1,8 +1,15 @@
-varibale "appName" {
+variable "appName" {
     type = string
     default = "INSERT APP NAME HERE"
 }
 
 variable "repoLink" {
     type = string
+}
+
+variable "PAT" {
+    type = string
+    default = ""
+
+    sensitive = true
 }
